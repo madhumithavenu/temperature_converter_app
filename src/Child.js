deb2f33096ed9764b15f557d9b52d73ef8375880
Madhumitha <madhumithavenu03@gmail.com>
2023-11-06 15:32:33 +0530

    Define input tag in child component

---
+++ src/Child.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Child(props){
+    function handleChange(e){
+        let num = null;
+        if(e.target.value === ''){
+            num = 0;
+        }else{
+            num = parseInt(e.target.value);
+        }
+        props.onTemperatureChange(num);
+    }
+
+    return(
+        <input
+        style={{"padding":"10px"}}
+        value = {props.temperature}
+        onChange={handleChange}></input>
+    )
+}
+export default Child;
