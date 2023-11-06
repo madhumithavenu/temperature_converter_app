5786b3279a5a0f671173c12b46d8da6015f03c4c
Madhumitha <madhumithavenu03@gmail.com>
2023-11-06 15:26:37 +0530

    Setting up the project

--- src/App.js
+++ src/App.js
@@ -2,23 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const celsius =0;
+    const fahrenheit =0;
+    const kelvin =0;
+
+    function onCelsiusChange(){
+
+    }
+    function onFahrenheitChange(){
+
+    }
+    function onKelvinChange(){
+
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{"textAlign":"center"}}>
+      <h2>Celsius: <Child temperature={celsius} onTemperatureChange={onCelsiusChange} /> </h2>
+      <h2>Fahrenheit: <Child temperature={fahrenheit} onTemperatureChange={onFahrenheitChange} /> </h2>
+      <h2>Kelvin: <Child temperature={kelvin} onTemperatureChange={onKelvinChange} /> </h2>
     </div>
+    
   );
 }
 
